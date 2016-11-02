66a91cac7ba386f680eafde8e8c4bc073106c17b
JEBernard <bernard.jason.e@gmail.com>
2016-11-01 21:39:56 -0500

    1

--- Bernard_Final_FitnessTracker/My Project/Application.Designer.vb
+++ Bernard_Final_FitnessTracker/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Bernard_Final_FitnessTracker.Form1
+            Me.MainForm = Global.Bernard_Final_FitnessTracker.frmLogin
         End Sub
     End Class
 End Namespace
