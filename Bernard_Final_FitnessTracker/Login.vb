5a9f996ab38dfea9d8f796241f1ca928210b6bda
JEBernard <bernard.jason.e@gmail.com>
2016-11-27 11:52:44 -0600

    Database: Registration & Login Login validation

--- Bernard_Final_FitnessTracker/Login.vb
+++ Bernard_Final_FitnessTracker/Login.vb
@@ -1,37 +1,74 @@
-﻿Public Class frmLogin
+﻿Imports System.Data.SqlClient
+
+Public Class frmLogin
     'Login and Register form
     Dim users As New Dictionary(Of String, String)
 
+    Dim connection As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Users.mdf;Integrated Security=True"
+
+    Dim myConn As New SqlConnection(connection)
+
     Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        users.Add("Jason", "Password")
+
     End Sub
 
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim strName As String = txtLoginUsername.Text
+        Dim strPassword As String = txtLoginPassword.Text
+        Dim count As Integer
+        Dim comm As New SqlCommand()
+        Dim query As String = "SELECT COUNT(*) FROM Users
+WHERE UserName = @UserName AND  Password = @Password"
 
-        Dim password As String = txtRegPassword.Text
-        If (users.TryGetValue(txtLoginUsername.Text, password) AndAlso password = txtLoginPassword.Text) Then
-            MessageBox.Show("Login successful")
+        With comm
+            .Connection = myConn
+            .CommandType = CommandType.Text
+            .CommandText = query
+            .Parameters.AddWithValue("@UserName", strName)
+            .Parameters.AddWithValue("@Password", strPassword)
+        End With
 
-            'Launch Profile form
-            Profile.ShowDialog()
-            Me.Close()
-        Else
-            MessageBox.Show("Invalid Username/Password")
-        End If
+        Try
+            myConn.Open()
+
+            count = comm.ExecuteScalar()
+
+            If count > 0 Then
+                MessageBox.Show("Login successfull")
+                myConn.Close()
+            Else
+                MessageBox.Show("Invalid Username/Password")
+                myConn.Close()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Private Sub btnRegister_Click(sender As Object, e As EventArgs) Handles btnRegister.Click
+        Dim strName As String = txtRegUsername.Text
+        Dim strPassword As String = txtRegPassword.Text
+        Dim query As String = "INSERT INTO Users
+                         (UserName, Password)
+VALUES        (@UserName,@Password)"
 
-        Dim password As String = txtRegPassword.Text
-        If (users.TryGetValue(txtRegUsername.Text, txtRegPassword.Text)) Then
-            MessageBox.Show("Username already exists please use the login screen")
-        Else
-            users.Add(txtRegUsername.Text, password)
-            MessageBox.Show("User has been registered successfully")
-        End If
-
-
-
+        Dim comm As New SqlCommand()
+        With comm
+            .Connection = myConn
+            .CommandType = CommandType.Text
+            .CommandText = query
+            .Parameters.AddWithValue("@Username", strName)
+            .Parameters.AddWithValue("@Password", strPassword)
+        End With
+        Try
+            myConn.Open()
+            comm.ExecuteNonQuery()
+            MessageBox.Show("New user: " & strName & " has been registered")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        myConn.Close()
 
     End Sub
 End Class
--- Bernard_Final_FitnessTracker/UsersDataSet1.Designer.vb
+++ Bernard_Final_FitnessTracker/UsersDataSet1.Designer.vb
@@ -927,11 +927,24 @@ Namespace UsersDataSEtTableAdapters
         <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")>  _
         Private Sub InitCommandCollection()
-            Me._commandCollection = New Global.System.Data.SqlClient.SqlCommand(0) {}
+            Me._commandCollection = New Global.System.Data.SqlClient.SqlCommand(2) {}
             Me._commandCollection(0) = New Global.System.Data.SqlClient.SqlCommand()
             Me._commandCollection(0).Connection = Me.Connection
             Me._commandCollection(0).CommandText = "SELECT Id, UserName, Password, Height, Weight, [Goal Weight] FROM dbo.Users"
             Me._commandCollection(0).CommandType = Global.System.Data.CommandType.Text
+            Me._commandCollection(1) = New Global.System.Data.SqlClient.SqlCommand()
+            Me._commandCollection(1).Connection = Me.Connection
+            Me._commandCollection(1).CommandText = "INSERT INTO [dbo].[Users] ([UserName], [Password])"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&" VALUES (@UserName, @Password"& _ 
+                ");"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)
+            Me._commandCollection(1).CommandType = Global.System.Data.CommandType.Text
+            Me._commandCollection(1).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@UserName", Global.System.Data.SqlDbType.NVarChar, 50, Global.System.Data.ParameterDirection.Input, 0, 0, "UserName", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
+            Me._commandCollection(1).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@Password", Global.System.Data.SqlDbType.NChar, 10, Global.System.Data.ParameterDirection.Input, 0, 0, "Password", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
+            Me._commandCollection(2) = New Global.System.Data.SqlClient.SqlCommand()
+            Me._commandCollection(2).Connection = Me.Connection
+            Me._commandCollection(2).CommandText = "SELECT COUNT(*) FROM Users"&Global.Microsoft.VisualBasic.ChrW(13)&Global.Microsoft.VisualBasic.ChrW(10)&"WHERE UserName = @UserName AND  Password = @Password"
+            Me._commandCollection(2).CommandType = Global.System.Data.CommandType.Text
+            Me._commandCollection(2).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@UserName", Global.System.Data.SqlDbType.NVarChar, 50, Global.System.Data.ParameterDirection.Input, 0, 0, "UserName", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
+            Me._commandCollection(2).Parameters.Add(New Global.System.Data.SqlClient.SqlParameter("@Password", Global.System.Data.SqlDbType.NChar, 10, Global.System.Data.ParameterDirection.Input, 0, 0, "Password", Global.System.Data.DataRowVersion.Current, false, Nothing, "", "", ""))
         End Sub
         
         <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
@@ -1107,6 +1120,74 @@ Namespace UsersDataSEtTableAdapters
          Global.System.ComponentModel.DataObjectMethodAttribute(Global.System.ComponentModel.DataObjectMethodType.Update, true)>  _
         Public Overloads Overridable Function Update(ByVal UserName As String, ByVal Password As String, ByVal Height As Integer, ByVal Weight As Integer, ByVal Goal_Weight As Integer, ByVal Original_Id As Integer, ByVal Original_UserName As String, ByVal Original_Password As String, ByVal Original_Height As Integer, ByVal Original_Weight As Integer, ByVal Original_Goal_Weight As Integer) As Integer
             Return Me.Update(UserName, Password, Height, Weight, Goal_Weight, Original_Id, Original_UserName, Original_Password, Original_Height, Original_Weight, Original_Goal_Weight, Original_Id)
+        End Function
+        
+        <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0"),  _
+         Global.System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter"),  _
+         Global.System.ComponentModel.DataObjectMethodAttribute(Global.System.ComponentModel.DataObjectMethodType.Insert, false)>  _
+        Public Overloads Overridable Function Register(ByVal UserName As String, ByVal Password As String) As Integer
+            Dim command As Global.System.Data.SqlClient.SqlCommand = Me.CommandCollection(1)
+            If (UserName Is Nothing) Then
+                Throw New Global.System.ArgumentNullException("UserName")
+            Else
+                command.Parameters(0).Value = CType(UserName,String)
+            End If
+            If (Password Is Nothing) Then
+                Throw New Global.System.ArgumentNullException("Password")
+            Else
+                command.Parameters(1).Value = CType(Password,String)
+            End If
+            Dim previousConnectionState As Global.System.Data.ConnectionState = command.Connection.State
+            If ((command.Connection.State And Global.System.Data.ConnectionState.Open)  _
+                        <> Global.System.Data.ConnectionState.Open) Then
+                command.Connection.Open
+            End If
+            Dim returnValue As Integer
+            Try 
+                returnValue = command.ExecuteNonQuery
+            Finally
+                If (previousConnectionState = Global.System.Data.ConnectionState.Closed) Then
+                    command.Connection.Close
+                End If
+            End Try
+            Return returnValue
+        End Function
+        
+        <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0"),  _
+         Global.System.ComponentModel.Design.HelpKeywordAttribute("vs.data.TableAdapter")>  _
+        Public Overloads Overridable Function ScalarQuery(ByVal UserName As String, ByVal Password As String) As Global.System.Nullable(Of Integer)
+            Dim command As Global.System.Data.SqlClient.SqlCommand = Me.CommandCollection(2)
+            If (UserName Is Nothing) Then
+                Throw New Global.System.ArgumentNullException("UserName")
+            Else
+                command.Parameters(0).Value = CType(UserName,String)
+            End If
+            If (Password Is Nothing) Then
+                Throw New Global.System.ArgumentNullException("Password")
+            Else
+                command.Parameters(1).Value = CType(Password,String)
+            End If
+            Dim previousConnectionState As Global.System.Data.ConnectionState = command.Connection.State
+            If ((command.Connection.State And Global.System.Data.ConnectionState.Open)  _
+                        <> Global.System.Data.ConnectionState.Open) Then
+                command.Connection.Open
+            End If
+            Dim returnValue As Object
+            Try 
+                returnValue = command.ExecuteScalar
+            Finally
+                If (previousConnectionState = Global.System.Data.ConnectionState.Closed) Then
+                    command.Connection.Close
+                End If
+            End Try
+            If ((returnValue Is Nothing)  _
+                        OrElse (returnValue.GetType Is GetType(Global.System.DBNull))) Then
+                Return New Global.System.Nullable(Of Integer)()
+            Else
+                Return New Global.System.Nullable(Of Integer)(CType(returnValue,Integer))
+            End If
         End Function
     End Class
     
